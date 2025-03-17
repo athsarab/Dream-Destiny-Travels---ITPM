af9c470e6722b06c06f3de17c878354cd2d659d5
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2025-03-17 15:01:30 +0530

    create api.js file

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';
+
+export const api = {
+  // Package APIs
+  getPackages: () => axios.get(`${API_URL}/packages`),
+  getPackage: (id) => axios.get(`${API_URL}/packages/${id}`),
+  createPackage: (data) => axios.post(`${API_URL}/packages`, data),
+  updatePackage: (id, data) => axios.put(`${API_URL}/packages/${id}`, data),
+  deletePackage: (id) => axios.delete(`${API_URL}/packages/${id}`),
+
+  // Custom Package APIs
+  getCategories: () => axios.get(`${API_URL}/custom-packages/categories`),
+  createCustomPackage: (data) => axios.post(`${API_URL}/custom-packages/options`, data),
+  deleteCategory: (id) => axios.delete(`${API_URL}/custom-packages/categories/${id}`)
+};
+
+export default api;
