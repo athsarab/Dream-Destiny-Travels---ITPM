a446caae213fdff83ffe04fc00781b43dee1abfe
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2025-03-21 14:04:03 +0530

    css

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -60,7 +60,7 @@ const AdminDashboard = () => {
           <h1 className="text-4xl font-bold text-transparent bg-clip-text 
                        bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500
                        drop-shadow-lg">
-            Package Management Dashboard
+            Package Management Dashboard 
           </h1>
           <div className="flex gap-4">
             <button
